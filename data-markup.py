b62c6c30a0c469f98cd3988404deeb046f1d802b
Timofey Nazarov <timothy96@rambler.ru>
2019-03-22 23:44:06 +0300

    shell config

--- data-markup.py
+++ data-markup.py
@@ -1,2 +1,12 @@
+from app import app, db
+from app.models import User, Profile, Profileclass, Vote
 
-from app import app
+@app.shell_context_processor
+def make_shell_context():
+	return {
+		'db': db,
+		'User': User,
+		'Profile': Profile,
+		'Profileclass': Profileclass,
+		'Vote': Vote
+	}
