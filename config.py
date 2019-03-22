0494ff0d934bfed20be6e129391c50e68e92cc15
Timofey Nazarov <timothy96@rambler.ru>
2019-03-22 19:14:32 +0300

    db configured and user model created

--- config.py
+++ config.py
@@ -5,5 +5,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     # Database connection
-    SQLALCHEMY_DATABASE_URI = os.environ.get(DATABASE_URL) or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
