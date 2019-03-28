de7b6bba9891820e9b5faf7c581e31ed2bdfa4ed
Timofey Nazarov <timothy96@rambler.ru>
2019-03-28 23:46:13 +0300

    old code deleted

--- app/routes.py
+++ app/routes.py
@@ -5,16 +5,6 @@ from app.models import User
 
 inputForm = 'server'
 
-@app.route('/')
-# single page app handled by Vue.js
-def index():
-	return app.send_static_file("index.html")
-
-@app.route('/dist/<path:path>')
-# serving static files
-def static_dist(path):
-	return send_from_directory("static/dist", path)
-
 @app.route('/api/properties')
 def properties():
 	return jsonify({
