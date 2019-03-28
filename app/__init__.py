7caf09257570a669a678c7440aa11a0408d4452b
Timofey Nazarov <timothy96@rambler.ru>
2019-03-28 20:08:40 +0300

    imported flask-login

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from lask_login import LoginManager
 
 app = Flask(__name__, static_folder='static/dist')
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 from app import routes, models
