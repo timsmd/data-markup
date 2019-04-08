3fb2462139e12fe64a6f833fd5b94da7c70a5c81
Timofey Nazarov <timothy96@rambler.ru>
2019-04-09 00:37:21 +0300

    refactoring vue structure

--- static/src/App.vue
+++ static/src/App.vue
@@ -1,23 +1,6 @@
 <template>
-	<div id="app">
-		<h1>App says: {{ msg }}</h1>
-		<p>login form</p>
-		<input v-model="username" placeholder="username">
-		<input v-model="password" placeholder="password">
-		<button v-on:click="login">Login</button>
-		<button v-on:click="logout">Logout</button>
-		<p>sign up form</p>
-		<button v-on:click="signup">Sign up</button>
-		<p>voting classes</p>
-		<div v-for="item in this.profile_classes">
-			<input type="radio" v-bind:id="item.id" value="Один" v-bind:name="item.id+'_'">
-			<label for="one">{{ item.if_true }}</label>
-			<input type="radio" v-bind:id="item.id" value="Два" v-bind:name="item.id+'_'">
-			<label for="two">{{ item.if_false }}</label>
-			<br>
-		</div>
-		<button v-on:click="get_profile">get profiles</button>
-		<button v-on:click="vote">test_vote</button>
+	<div id='app'>
+			<vote-class id='vote-class'></vote-class>
 	</div>
 </template>
 
@@ -25,9 +8,11 @@
  	import axios from 'axios';
  	export default {
 		name: 'app',
-		data() {
+		data: function () {
 			return {
 				msg: '',
+				current_user: '',
+				logged_in: '',
 				username: '',
 				password: '',
 				profiles: [],
@@ -47,15 +32,12 @@
 			}
 		},
 		created: function () {
-				axios.get('/api/classes')
-				.then(response => {
-					this.profile_classes = response.data.classes
-				})
-				.catch(e => {
-					this.errors.push(e)
-				})
-			},
-		// get classes from db on created page
+			axios.get('/api/check/login')
+			.then(response => {
+				this.logged_in = response.data.logged_in;
+				this.current_user = response.data.username || null;
+			})
+		},
 		methods: {
 			login: function () {
 				if (this.username != '' && this.password != '') {
@@ -65,7 +47,9 @@
 					})
 					.then(response => {
 						if (response.data.logged_in) {
-							this.msg = response.data.username + ' logged in'
+							this.logged_in = true;
+							this.current_user = response.data.username;
+							this.msg = this.current_user + ' logged in';
 						}
 					})
 					.catch(e => {
@@ -90,6 +74,7 @@
 			logout: function () {
 				axios.get('/api/logout')
 				.then(response => {
+					// TODO handle data(logged in flag & current user) on logout
 					this.msg = response.data.logged_out
 				})
 				.catch(e => {
@@ -105,15 +90,6 @@
 					//
 				})
 			},
-			get_profile: function () {
-				axios.get('/api/profile')
-				.then(response => {
-					this.profile_classes = response.data.classes
-				})
-				.catch(e => {
-					this.errors.push(e)
-				})
-			}
 		}
 	}
 </script>
