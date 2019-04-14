15d8507e070b0899f821417be21d4874cd79c0b8
Timofey Nazarov <timothy96@rambler.ru>
2019-04-14 22:51:07 +0300

    fixed batch upload

--- backend/tools/batch-upload.py
+++ backend/tools/batch-upload.py
@@ -1,4 +1,9 @@
 import sys
+import os.path
+
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
 from app import app, db
 from app.models import Batch, Profile
 
